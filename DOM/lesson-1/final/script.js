b7cc63b378ba6bd78475664fac5269231e0340c4
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2023-10-28 19:46:28 +0000

    Update script.js

--- DOM/lesson-1/final/script.js
+++ DOM/lesson-1/final/script.js
@@ -1,42 +1,36 @@
-// how to select an element 
-const [firstBox] = document.getElementsByClassName('box-1')
-const secondBox = document.querySelector('.box-2')
-const thirdBox = document.getElementById('box-3')
-const allBoxes = document.querySelectorAll('.box')
-console.log(allBoxes)
+// how to select an element
+const [firstBox] = document.getElementsByClassName("box-1");
+const secondBox = document.querySelector(".box-2");
+const thirdBox = document.getElementById("box-3");
+const allBoxes = document.querySelectorAll(".box");
+const container = document.querySelector(".box-container");
+console.log(allBoxes);
 // console.log(firstBox)
-console.log(secondBox)
+console.log(secondBox);
 // console.log(thirdBox)
 
 // console.log(firstBox[0])
 
-
-
 //2 How to change the content of HTML elements
-firstBox.textContent= 'happy'
-firstBox.style.backgroundColor ='grey'
-firstBox.style.color ='brown'
+firstBox.textContent = "happy";
+firstBox.style.backgroundColor = "grey";
+firstBox.style.color = "brown";
 
-console.log(secondBox)
+console.log(secondBox);
 // firstBox.style.width ='30%'
 
 // responding to events
-allBoxes.forEach(box=>box.addEventListener('click',function(e){
-const diffColor= function() { return Math.floor((Math.random()*255)+1)}
-e.target.style.backgroundColor=`rgb(${diffColor()}, ${diffColor()},${diffColor()} )`
-}))
+const diffColor = function () {
+  return Math.floor(Math.random() * 255 + 1);
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// allBoxes.forEach((box) =>
+//   box.addEventListener("click", function (e) {
+//     e.target.style.backgroundColor = `rgb(${diffColor()}, ${diffColor()},${diffColor()} )`;
+//   })
+// );
+container.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (!e.target.classList.contains("box")) return;
+  e.target.style.backgroundColor = `rgb(${diffColor()}, ${diffColor()},${diffColor()} )`;
+});
