993e0238cd469c6abe80788f415c3d26d67c73be
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2023-10-29 14:11:18 +0000

    Update script.js

--- DOM/lesson-1/starter/script.js
+++ DOM/lesson-1/starter/script.js
@@ -1,34 +1,50 @@
-//accessing page element
+// accessing dom elements
 const box_1 = document.getElementById("box-1");
-console.log(box_1);
-const box_2 = document.getElementsByClassName("box-2");
-console.log(box_2);
-const box_3 = document.querySelector("#box-3");
-console.log(box_3);
-const allBoxes = document.querySelectorAll(".box");
+const box_2 = document.querySelector(".box-2");
+const box_3 = document.querySelector(".box-3");
+const allBoxes = document.getElementsByClassName("box");
+const allBoxes2 = document.querySelectorAll(".box");
+const container = document.querySelector(".box-container");
 console.log(allBoxes);
-
 //manipulating dom elements
-// box_1.style.backgroundColor='blue'
-// box_1.style.width='100px'
-// allBoxes.forEach((box)=>box.style.color='black')
+//box_1.style.backgroundColor = "blue";
+//box_1.style.height = "100px";
+//box_1.style.opacity = 0.5;
 
-//manipulate elements based on events
-
-function generateRandomColor() {
-  function randomNumber() {
-    return Math.floor(Math.random() * 255 + 1);
-  }
-
-  return `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+for (let i = 0; i < allBoxes2.length; i++) {
+  allBoxes2[i].style.backgroundColor = "blue";
 }
 
-// box_1.addEventListener('click',function(){
-//     box_1.style.backgroundColor=generateRandomColor()
-// })
+//allBoxes2.forEach(function (box) {
+// return (box.style.backgroundColor = "green");
+//});
 
-allBoxes.forEach((box) => {
-  box.addEventListener("click", function () {
-    box.style.backgroundColor = generateRandomColor();
-  });
+//box_2.innerHTML = "how are you";
+//box_3.textContent = "happy";
+
+//events
+
+function generateRandomNumber() {
+  let number = Math.floor(Math.random() * 255 + 1);
+  return number;
+}
+
+//allBoxes2.forEach(function (box) {
+// box.addEventListener("click", function (event) {
+//  event.target.style.backgroundColor = `rgb(${generateRandomNumber()}, ${generateRandomNumber()}, ${generateRandomNumber()})`;
+// });
+//});
+
+container.addEventListener("click", function (event) {
+  if (!event.target.classList.contains("box")) return;
+  event.target.style.backgroundColor = `rgb(${generateRandomNumber()}, ${generateRandomNumber()}, ${generateRandomNumber()})`;
 });
+
+//box_1.addEventListener("click", function (event) {
+//event.target.style.backgroundColor = `rgb(${generateRandomNumber()}, ${generateRandomNumber()}, ${generateRandomNumber()})`;
+//});
+//const num1 = 2;
+//const num2 = 3;
+//const sum = num1 + num2;
+//console.log(num1 + "+" + num2 + "=" + sum);
+//console.log(`${num1} +  ${num2} = ${sum}`);
