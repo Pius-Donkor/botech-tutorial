953763ed1e823426255e1e3ae774af98bf87296d
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2023-10-29 23:41:06 +0000

    Update script.js

--- asynchronous javascript/final/script.js
+++ asynchronous javascript/final/script.js
@@ -1,11 +1,31 @@
 //callback function
-function generateRandomNumber(maxNumber) {
-  console.log(Math.floor(Math.random() * maxNumber + 1));
+// function generateRandomNumber(maxNumber) {
+//   console.log(Math.floor(Math.random() * maxNumber + 1));
+// }
+
+// function addNumbers(num1, num2, callback) {
+//   let sum = num1 + num2;
+//   return callback(sum);
+// }
+
+// addNumbers(2, 3, generateRandomNumber);
+
+const fruits = ["mango", "pawpaw", "apple ", "grapes"];
+function getFruits() {
+  setTimeout(() => {
+    let output = "";
+    fruits.forEach((item) => {
+      output += `<li>${item}</li>`;
+    }, 1000);
+    document.body.innerHTML = output;
+  });
 }
 
-function addNumbers(num1, num2, callback) {
-  let sum = num1 + num2;
-  return callback(sum);
+function createFruits(fruit, callback) {
+  setTimeout(() => {
+    fruits.push(fruit);
+    callback();
+  }, 2000);
 }
 
-addNumbers(2, 3, generateRandomNumber);
+createFruits("avocado", getFruits);
