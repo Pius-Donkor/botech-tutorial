e1a4f9258ff43c912fcf4dc152dc43729adcdf16
Pius <107971234+Pius-Donkor@users.noreply.github.com>
2023-11-04 09:47:08 +0000

    Update script.js

--- Arrays,Objects,Async/Arrays and objects/final/script.js
+++ Arrays,Objects,Async/Arrays and objects/final/script.js
@@ -1,9 +1,71 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-fruits.splice(2, 1, "Lemon", "Kiwi");
-console.log(fruits);
-fruits.splice(0, 2);
-console.log(fruits);
+//array
+// const fruits = ["orange", "apple", "pineapple", "mango"];
+// console.log(fruits.length);
+// const orange = fruits[1];
+// console.log(fruits[2]);
 
-const fruits2 = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-const citrus = fruits2.slice(3);
-console.log(citrus);
+//adding elements to an array
+//fruits[4] = "avocado";
+//console.log(fruits);
+
+//fruits.push("avocado");
+//console.log(fruits);
+
+//unshift method
+//fruits.unshift("tangerine");
+//console.log(fruits);
+
+// deleting elements from an array
+
+//pop
+//console.log(fruits.pop());
+//console.log(fruits);
+
+//shift
+//console.log(fruits.shift());
+
+//iteration methods
+//fruits.forEach(function(value,index,array){
+//   console.log(value,index,array);
+//})
+
+//map method
+// const newFruits = fruits.map((value) => {
+//   return value + " sweet";
+// });
+
+// console.log(newFruits);
+
+// filter method
+// const filteredFruits = fruits.filter((value) => value !== "apple");
+// console.log(filteredFruits);
+
+//reduce method
+// const numbers = [1, 2, 3, 4, 5];
+// const addNum = numbers.reduce((acc, value) => {
+//   return acc + value;
+// }, 0);
+// console.log(addNum);
+
+//objects
+const person = {
+  name: "pius",
+  address: "P.O.BOX 322",
+  country: "Ghana",
+};
+
+console.log(person);
+
+//accessing properties of an object
+
+const name = person.name;
+console.log(name);
+console.log(person.address);
+const country = person["country"];
+console.log(country);
+
+//adding to an object
+person.height = "tall";
+person["complexion"] = "fair";
+person.name = "Donkor";
+console.log(person);
